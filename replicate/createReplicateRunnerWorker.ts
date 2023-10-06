161382991d6fa449f3047570a42159611686be58
Emily Wang <emily@zztech.io>
2023-10-05 20:28:36 -0400

    Add createReplicateRunnerWorker function to the codebase
    - Added dependencies required for createReplicateRunnerWorker function
    - Created a new file createReplicateRunnerWorker.ts
    - Implemented createReplicateRunnerWorker function
    - Configured function parameters and added necessary validations
    - Integrated Replicate API to process job data
    - Updated the database with job result and status
    - Returned job result from the function

---
+++ replicate/createReplicateRunnerWorker.ts
@@ -0,0 +1,62 @@
+import { createWorkerMainFunction } from "../microworkers/worker-creator";
+import Replicate from "replicate";
+import { GenericRecordType, QueueName } from "../microworkers/workerCommon";
+import { Knex } from "knex";
+import { WorkerOptions } from "bullmq";
+
+if (!process.env.REPLICATE_API_TOKEN) {
+  throw new Error("REPLICATE_API_TOKEN not found");
+}
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+
+export function createReplicateRunnerWorker<
+  TJobData extends object,
+  TJobResult,
+  QueueNameDef extends GenericRecordType
+>({
+  endpoint,
+  queueName,
+  queueNamesDef,
+  projectId,
+  db,
+  workerOptions,
+}: {
+  queueName: QueueName<QueueNameDef>;
+  queueNamesDef: QueueNameDef;
+  endpoint: `${string}/${string}:${string}`;
+  projectId: string;
+  db: Knex;
+  workerOptions: WorkerOptions;
+}) {
+  return createWorkerMainFunction<
+    TJobData,
+    { replicateResult: TJobResult },
+    QueueNameDef
+  >({
+    queueName,
+    queueNamesDef,
+    projectId,
+    db,
+    workerOptions,
+    processor: async ({ job, logger, update }) => {
+      const input = job.data;
+      const result = (await replicate.run(endpoint, {
+        input: input,
+      })) as unknown as TJobResult;
+
+      await update({
+        incrementalData: {
+          replicateResult: result,
+          status: "FINISH",
+
+          updateTime: Date.now(),
+        },
+      });
+
+      return { replicateResult: result };
+    },
+    storageBucketName: "",
+  });
+}
