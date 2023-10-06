5f54bc88a18992f41f712f0feab90cf3b8142cc3
Emily Wang <emily@zztech.io>
2023-10-05 20:28:36 -0400

    add jobs dep table

--- migrations/20231001173939_create_jobs_log_table.js
+++ migrations/20231001173939_create_jobs_log_table.js
@@ -5,9 +5,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable("jobs_log", function (table) {
     table.primary(["project_id", "job_type", "job_id"]);
-    table.string("project_id");
-    table.string("job_type");
-    table.string("job_id");
+    table.string("project_id").notNullable();
+    table.string("job_type").notNullable();
+    table.string("job_id").notNullable();
     table.jsonb("job_data");
     table.timestamp("time_created").defaultTo(knex.fn.now());
     table.timestamp("time_updated").defaultTo(knex.fn.now());
--- migrations/20231111215102_create_job_deps_table.js
+++ migrations/20231111215102_create_job_deps_table.js
@@ -2,30 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-  return knex.schema
-    .createTable("job_deps", (table) => {
-      table.primary(["project_id", "parent_job_id", "child_job_id"]);
-      table.string("project_id");
-      table.string("parent_job_id").notNullable();
-      table.string("child_job_id").notNullable();
-      table.foreign("parent_job_id").references("jobs_log.job_id");
-      table.foreign("child_job_id").references("jobs_log.job_id");
-      table.unique(["project_id", "parent_job_id", "child_job_id"]);
-    })
-    .then(() => knex.schema.alterTable("jobs_log", (table) => {
-      table.unique("job_id");
-    }));
+exports.up = async function (knex) {
+  await knex.schema.alterTable("jobs_log", (table) => {
+    table.unique("job_id");
+  });
+  return await knex.schema.createTable("job_deps", (table) => {
+    table.primary(["project_id", "parent_job_id", "child_job_id"]);
+    table.string("project_id");
+    table.string("parent_job_id").notNullable();
+    table.string("child_job_id").notNullable();
+    table.foreign("parent_job_id").references("jobs_log.job_id");
+    table.foreign("child_job_id").references("jobs_log.job_id");
+    table.unique(["project_id", "parent_job_id", "child_job_id"]);
+  });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function (knex) {
-  return knex.schema
-    .dropTable("job_deps")
-    .then(() => knex.schema.alterTable("jobs_log", (table) => {
-      table.dropUnique("job_id");
-    }));
+exports.down = async function (knex) {
+  await knex.schema.dropTable("job_deps");
+  return await knex.schema.alterTable("jobs_log", (table) => {
+    table.dropUnique("job_id");
+  });
 };
