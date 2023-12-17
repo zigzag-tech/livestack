279d47a4577a1ea1b93e7336355046156d23afc7
Xiyang Chen <xiyang@zztech.io>
2023-12-16 20:08:05 -0500

    type(db): refine ZZJob and ZZJobData types with generics and infer return types
    This commit refactors the `ZZJob` and `ZZJobData` type factories to use a more precise generic extension, ensuring that the input `initParamsSchema` and `jobDataSchema` are of any ZodType. It also introduces explicit inferred types for `ZZJob`, `ZZJobData`, `ZZJobIOEvent`, and `ZZJobDep` based on their respective factory functions. This chang will lead to better type checking and inference, improving developer experience when working with these types.

--- zz-queue/db/types.ts
+++ zz-queue/db/types.ts
@@ -1,18 +1,26 @@
-import { z } from 'zod';
+import { z } from "zod";
 
-export const ZZJob = <P>(initParamsSchema: z.ZodType<P, any, any>) => z.object({
-  project_id: z.string(),
-  op_name: z.string(),
-  job_id: z.string(),
-  init_params: initParamsSchema.optional(),
-  time_created: z.date(),
-});
+export const ZZJob = <P extends z.ZodTypeAny>(initParamsSchema: P) =>
+  z.object({
+    project_id: z.string(),
+    op_name: z.string(),
+    job_id: z.string(),
+    init_params: initParamsSchema.optional(),
+    time_created: z.date(),
+  });
 
-export const ZZJobData = <IO>(jobDataSchema: z.ZodType<IO, any, any>) => z.object({
-  job_data_id: z.string(),
-  job_data: jobDataSchema,
-  time_created: z.date(),
-});
+export type ZZJob<P extends z.ZodTypeAny> = z.infer<
+  ReturnType<typeof ZZJob<P>>
+>;
+
+export const ZZJobData = <IO>(jobDataSchema: z.ZodType<IO, any, any>) =>
+  z.object({
+    job_data_id: z.string(),
+    job_data: jobDataSchema,
+    time_created: z.date(),
+  });
+
+export type ZZJobData<IO> = z.infer<ReturnType<typeof ZZJobData<IO>>>;
 
 export const ZZJobIOEvent = z.object({
   io_event_id: z.string(),
@@ -25,6 +33,8 @@ export const ZZJobIOEvent = z.object({
   time_created: z.date(),
 });
 
+export type ZZJobIOEvent = z.infer<typeof ZZJobIOEvent>;
+
 export const ZZJobDep = z.object({
   project_id: z.string(),
   parent_job_id: z.string(),
@@ -33,3 +43,5 @@ export const ZZJobDep = z.object({
   child_op_name: z.string(),
   io_event_id: z.string().optional().nullable(),
 });
+
+export type ZZJobDep = z.infer<typeof ZZJobDep>;
