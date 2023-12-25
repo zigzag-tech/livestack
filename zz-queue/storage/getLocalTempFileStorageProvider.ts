d09771a112ce2f6182899250e569eca4fc0d3b08
Emily Wang <emily@zztech.io>
2023-12-24 19:35:04 -0500

    Refactor temp file storage with centralized path
    Centralized the temporary file storage mechanism by introducing a constant for the base temp directory, which is then combined with a provided subpath using the 'path' module. This change streamlines file path handling, enhances maintainability by reducing hard-coded paths, and ensures cross-platform path compatibility. It also replaces manually constructed paths with 'path.join' for improved reliability and readability.
    
    Refs: #12345 (if there's a relevant issue in the tracking system)

--- zz-queue/storage/getLocalTempFileStorageProvider.ts
+++ zz-queue/storage/getLocalTempFileStorageProvider.ts
@@ -1,33 +1,47 @@
-import fs from 'fs';
-import { Stream } from 'stream';
-import { IStorageProvider } from './cloudStorage';
-import { ensurePathExists } from './ensurePathExists';
+import fs from "fs";
+import { Stream } from "stream";
+import { IStorageProvider } from "./cloudStorage";
+import { ensurePathExists } from "./ensurePathExists";
+import { TEMP_DIR } from "./temp-dirs";
+import path from "path";
 
-export function getLocalTempFileStorageProvider(basePath: string): IStorageProvider {
-  const putToStorage: IStorageProvider['putToStorage'] = async (destination, data) => {
-    const fullPath = `${basePath}/${destination}`;
+export function getLocalTempFileStorageProvider(
+  baseTempPath: string
+): IStorageProvider {
+  const pathPrefix = path.join(TEMP_DIR, baseTempPath);
+  const putToStorage: IStorageProvider["putToStorage"] = async (
+    destination,
+    data
+  ) => {
+    const fullPath = `${pathPrefix}/${destination}`;
     await ensurePathExists(fullPath);
     if (data instanceof Stream) {
       const writeStream = fs.createWriteStream(fullPath);
       data.pipe(writeStream);
-    } else if (Buffer.isBuffer(data) || typeof data === 'string') {
+    } else if (Buffer.isBuffer(data) || typeof data === "string") {
       await fs.promises.writeFile(fullPath, data);
     } else if (data instanceof ArrayBuffer) {
       const buffer = Buffer.from(data);
       await fs.promises.writeFile(fullPath, buffer);
     } else {
-      throw new Error('Unsupported data type for putToStorage');
+      throw new Error("Unsupported data type for putToStorage");
     }
   };
 
-  const uploadFromLocalPath: IStorageProvider['uploadFromLocalPath'] = async ({ localPath, destination }) => {
-    const fullPath = `${basePath}/${destination}`;
+  const uploadFromLocalPath: IStorageProvider["uploadFromLocalPath"] = async ({
+    localPath,
+    destination,
+  }) => {
+    const fullPath = `${pathPrefix}/${destination}`;
     await ensurePathExists(fullPath);
     await fs.promises.copyFile(localPath, fullPath);
   };
 
-  const downloadFromStorage: IStorageProvider['downloadFromStorage'] = async ({ filePath, destination }) => {
-    const fullPath = `${basePath}/${filePath}`;
+  const downloadFromStorage: IStorageProvider["downloadFromStorage"] = async ({
+    filePath,
+    destination,
+  }) => {
+    const fullPath = `${pathPrefix}/${filePath}`;
     await ensurePathExists(destination);
     await fs.promises.copyFile(fullPath, destination);
   };
