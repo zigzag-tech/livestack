2aedbd27fee14b874cdf662a495d81f3110d7df3
Emily Wang <emily@zztech.io>
2023-12-29 13:17:22 -0500

    make ready for npm publish

--- zz-queue/index.ts
+++ zz-queue/index.ts
@@ -1,1 +1,5 @@
-console.log("capybara");
+import { ZZJob } from "./microworkers/ZZJob";
+import { ZZWorker } from "./microworkers/ZZWorker";
+import { ZZPipe } from "./microworkers/ZZPipe";
+
+export { ZZPipe, ZZWorker, ZZJob };
