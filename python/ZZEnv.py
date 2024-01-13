acc0f43168e62956d51b2a2a9475e54ec5af7b54
Xiyang Chen <xiyang@zztech.io>
2024-01-12 19:36:55 -0500

    use model_copy

--- python/ZZEnv.py
+++ python/ZZEnv.py
@@ -1,6 +1,7 @@
 from typing import Any, Optional
 from pydantic import BaseModel
 
+
 class ZZEnv(BaseModel):
     storage_provider: Optional[Any] = None
     project_id: str
@@ -11,10 +12,11 @@ class ZZEnv(BaseModel):
     def global_env(cls):
         # This method should return the global ZZEnv instance
         # The implementation depends on how the global environment is managed in the Python codebase
-        raise NotImplementedError("The method to retrieve the global ZZEnv instance needs to be implemented.")
+        raise NotImplementedError(
+            "The method to retrieve the global ZZEnv instance needs to be implemented.")
 
     def derive(self, **kwargs) -> 'ZZEnv':
         """
         Create a new ZZEnv instance by overriding some of the properties of the current environment.
         """
-        return self.copy(update=kwargs)
+        return self.model_copy(update=kwargs)
