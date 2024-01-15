5f6cf2af308ac11203a4f4f5f7e351ed78dd723e
Xiyang Chen <xiyang@zztech.io>
2024-01-15 02:07:37 -0500

    try new logger

--- core/utils/createWorkerLogger.ts
+++ core/utils/createWorkerLogger.ts
@@ -1,8 +1,39 @@
-import winston from "winston";
+import winston, { format } from "winston";
+import chalk from "chalk";
+
+const { combine, timestamp, label, printf, simple, splat } = format;
 
 const loggers: { [key: string]: winston.Logger } = {};
 
 export function getLogger(workerName: string, logColor?: string) {
+  const consoleFormat = printf(({ level, message, label, timestamp }) => {
+    var levelUpper = level.toUpperCase();
+    switch (levelUpper) {
+      case "INFO":
+        message = chalk.green(message);
+        level = chalk.black.bgGreenBright.bold(level);
+        break;
+
+      case "WARN":
+        message = chalk.yellow(message);
+        level = chalk.black.bgYellowBright.bold(level);
+        break;
+
+      case "ERROR":
+        message = chalk.red(message);
+        level = chalk.black.bgRedBright.bold(level);
+        break;
+
+      default:
+        break;
+    }
+    return `[${chalk.black.bgBlue.bold(
+      workerName
+    )}] [${chalk.black.bgWhiteBright(
+      timestamp
+    )}] [${level.toUpperCase()}]: ${message}`;
+  });
+
   if (!loggers[workerName]) {
     loggers[workerName] = winston.createLogger({
       level: "info",
@@ -13,15 +44,12 @@ export function getLogger(workerName: string, logColor?: string) {
         winston.format.timestamp({
           format: "YY-MM-DD HH:mm:ss",
         }),
-        winston.format.printf(
-          ({ level, message, timestamp }) =>
-            `[${workerName}] [${timestamp}] [${level.toUpperCase()}]: ${message}`
-        )
+        consoleFormat
       ),
       defaultMeta: { service: workerName },
       transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: `logs/${workerName}.log` }),
+        // new winston.transports.File({ filename: `logs/${workerName}.log` }),
       ],
     });
   }
