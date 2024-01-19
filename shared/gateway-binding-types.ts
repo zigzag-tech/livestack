1602a0cd460206c9437eaca8a99dfebc678d6809
Xiyang Chen <xiyang@zztech.io>
2024-01-18 22:52:19 -0500

    reporg

--- shared/gateway-binding-types.ts
+++ shared/gateway-binding-types.ts
@@ -1,9 +1,8 @@
+export const REQUEST_AND_BIND_CMD = "request_and_bind";
 export type RequestAndBindType = {
   specName: string;
   uniqueSpecLabel?: string;
 };
-
-export const REQUEST_AND_BIND_CMD = "request_and_bind";
 
 export const JOB_INFO = "job_info";
 
