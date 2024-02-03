a75c679d2de838aeab09e115806e524c1b83d2eb
Xiyang Chen <xiyang@zztech.io>
2024-02-03 02:15:08 -0500

    Add RawQueueJobData type to workerCommon.ts

--- packages/core/orchestrations/workerCommon.ts
+++ packages/core/orchestrations/workerCommon.ts
@@ -1,2 +1,6 @@
 export type GenericRecordType = { [x: string]: string };
 export type QueueName<T extends GenericRecordType> = T[keyof T];
+export type RawQueueJobData<JobOptions> = {
+  jobOptions: JobOptions;
+  contextId: string | null;
+};
