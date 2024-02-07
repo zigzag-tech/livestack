2d7251503f4e92e5e6b50d7942ee61af413b1502
Xiyang Chen <xiyang@zztech.io>
2024-02-06 20:51:36 -0500

    basic

--- packages/vault-interface/build_proto.bash
+++ packages/vault-interface/build_proto.bash
@@ -4,6 +4,6 @@ OUT_DIR="./src/generated"
 
 protoc --plugin=$(npm root)/.bin/protoc-gen-ts_proto \
  --ts_proto_out="${OUT_DIR}" \
- --ts_proto_opt=outputServices=grpc-js \
+ --ts_proto_opt=outputServices=default \
  --ts_proto_opt=esModuleInterop=true \
  -I=src/ src/*.proto
