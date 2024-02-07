688c0dcc41d8bdd94b1d9a5be6b2e9e82845a4b3
Xiyang Chen <xiyang@zztech.io>
2024-02-07 00:47:33 -0500

    Add QueueService in vault-interface package

---
+++ packages/vault-interface/src/generated/queue.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+export const protobufPackage = "livestack";
+
+export type QueueServiceDefinition = typeof QueueServiceDefinition;
+export const QueueServiceDefinition = {
+  name: "QueueService",
+  fullName: "livestack.QueueService",
+  methods: {},
+} as const;
+
+export interface QueueServiceImplementation<CallContextExt = {}> {
+}
+
+export interface QueueServiceClient<CallOptionsExt = {}> {
+}
