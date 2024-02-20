b31b1a31317e63496881e9fd84a58026250b8ea5
Xiyang Chen <xiyang@zztech.io>
2024-02-20 04:47:29 -0500

    Update testing from mocha to jest in shared package

--- packages/shared/src/graph/DefGraph.test.ts
+++ packages/shared/src/graph/DefGraph.test.ts
@@ -1,27 +1,46 @@
-import { DefGraph } from './DefGraph';
-import { expect } from 'chai';
+import { DefGraph } from "./DefGraph";
 
-describe('DefGraph', () => {
-  it('should create a root spec node', () => {
-    const rootSpec = { name: 'RootSpec', inputDefSet: { tags: [] }, outputDefSet: { tags: [] } };
+describe("DefGraph", () => {
+  it("should create a root spec node", () => {
+    const rootSpec = {
+      name: "RootSpec",
+      inputDefSet: { tags: [] },
+      outputDefSet: { tags: [] },
+    };
     const graph = new DefGraph({ root: rootSpec });
     const rootSpecNodeId = graph.getRootSpecNodeId();
     const rootSpecNode = graph.getNodeAttributes(rootSpecNodeId);
-    expect(rootSpecNode).to.have.property('nodeType', 'root-spec');
-    expect(rootSpecNode).to.have.property('specName', rootSpec.name);
+    expect(rootSpecNode).toHaveProperty("nodeType", "root-spec");
+    expect(rootSpecNode).toHaveProperty("specName", rootSpec.name);
   });
 
-  it('should add connected dual specs', () => {
-    const graph = new DefGraph({ root: { name: 'RootSpec', inputDefSet: { tags: [] }, outputDefSet: { tags: [] } } });
-    const from = { specName: 'SpecA', output: 'outputA' };
-    const to = { specName: 'SpecB', input: 'inputB', hasTransform: false };
+  it("should add connected dual specs", () => {
+    const graph = new DefGraph({
+      root: {
+        name: "RootSpec",
+        inputDefSet: { tags: [] },
+        outputDefSet: { tags: [] },
+      },
+    });
+    const from = { specName: "SpecA", output: "outputA" };
+    const to = { specName: "SpecB", input: "inputB", hasTransform: false };
     graph.addConnectedDualSpecs(from, to);
-    const fromSpecNodeId = graph.findNode((_, attrs) => attrs.nodeType === 'spec' && attrs.specName === from.specName);
-    const toSpecNodeId = graph.findNode((_, attrs) => attrs.nodeType === 'spec' && attrs.specName === to.specName);
-    expect(fromSpecNodeId).to.not.be.null;
-    expect(toSpecNodeId).to.not.be.null;
-    expect(graph.hasEdge(fromSpecNodeId, toSpecNodeId)).to.be.true;
+    const fromSpecNodeId = graph.findNode(
+      (_, attrs) =>
+        attrs.nodeType === "spec" && attrs.specName === from.specName
+    );
+    const toSpecNodeId = graph.findNode(
+      (_, attrs) => attrs.nodeType === "spec" && attrs.specName === to.specName
+    );
+    expect(fromSpecNodeId).not.toBeNull();
+    expect(toSpecNodeId).not.toBeNull();
+    w;
+    const fromOutletNodeId = graph.outboundNeighbors(fromSpecNodeId!)[0];
+    expect(fromOutletNodeId).not.toBeNull();
+    const streamNodeId = graph.outboundNeighbors(fromOutletNodeId!)[0];
+    expect(streamNodeId).not.toBeNull();
+    const toInletNodeId = graph.outboundNeighbors(streamNodeId!)[0];
+    expect(toInletNodeId).not.toBeNull();
+    expect(graph.hasEdge(toInletNodeId!, toSpecNodeId!)).toBe(true);
   });
-
-  // Additional test cases should be added here to cover more functionality
 });
