ffbaf25ffdd614926689c7f7282673f6142421af
Xiyang Chen <xiyang@zztech.io>
2024-02-20 22:58:52 -0500

    Added assert_matches crate as a dependency and updated test in def_graph.rs.

--- crosslang/shared/src/systems/def_graph.rs
+++ crosslang/shared/src/systems/def_graph.rs
@@ -1,29 +1,6 @@
 use petgraph::graph::{DiGraph, NodeIndex};
 use std::collections::HashMap;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use assert_matches::assert_matches;
-
-    #[test]
-    fn test_ensure_node() {
-        let mut graph = DefGraph::<&str>::new();
-
-        // Ensure a node is created
-        let node_id = graph.ensure_node("TestNode", "TestData");
-        assert_matches!(graph.graph.node_weight(node_id), Some(&"TestData"));
-
-        // Ensure the same node is retrieved with the same ID
-        let same_node_id = graph.ensure_node("TestNode", "TestData");
-        assert_eq!(node_id, same_node_id);
-
-        // Ensure the node data is not overwritten if the same ID is used
-        let same_node_id_with_different_data = graph.ensure_node("TestNode", "DifferentData");
-        assert_matches!(graph.graph.node_weight(same_node_id_with_different_data), Some(&"TestData"));
-    }
-}
-
 pub struct DefGraph<N> {
     graph: DiGraph<N, ()>,
     node_indices: HashMap<String, NodeIndex>,
@@ -46,5 +23,31 @@ impl<N> DefGraph<N> {
                 index
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_matches::assert_matches;
+
+    #[test]
+    fn test_ensure_node() {
+        let mut graph = DefGraph::<&str>::new();
+
+        // Ensure a node is created
+        let node_id = graph.ensure_node("TestNode", "TestData");
+        assert_matches!(graph.graph.node_weight(node_id), Some(&"TestData"));
+
+        // Ensure the same node is retrieved with the same ID
+        let same_node_id = graph.ensure_node("TestNode", "TestData");
+        assert_eq!(node_id, same_node_id);
+
+        // Ensure the node data is not overwritten if the same ID is used
+        let same_node_id_with_different_data = graph.ensure_node("TestNode", "DifferentData");
+        assert_matches!(
+            graph.graph.node_weight(same_node_id_with_different_data),
+            Some(&"TestData")
+        );
     }
 }
