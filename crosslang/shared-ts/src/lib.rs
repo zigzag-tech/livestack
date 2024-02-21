179e67db23e951abf9dd8b1bac0ddc4ae8d23ed4
Xiyang Chen <xiyang@zztech.io>
2024-02-21 18:36:44 -0500

    Removed `def_graph` function from lib.rs.

--- crosslang/shared-ts/src/lib.rs
+++ crosslang/shared-ts/src/lib.rs
@@ -11,14 +11,3 @@ pub fn sum_as_string(a: i32, b: i32) -> String {
   return sum_as_string_impl(a as usize, b as usize);
 }
 
-#[napi]
-pub fn def_graph() -> u32  {
-  let root_spec = SpecBase {
-    name: "RootSpec".to_string(),
-    input_tags: vec![],
-    output_tags: vec![],
-};
-   let g = DefGraph::new(root_spec);
-   return g.graph.node_count().try_into().unwrap();
-
-}
