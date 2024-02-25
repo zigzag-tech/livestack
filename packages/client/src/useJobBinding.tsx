7dcdda951440dbae9c818276e7afb0c0ae8ad150
Xiyang Chen <xiyang@zztech.io>
2024-02-25 05:58:25 -0500

    Refactor useJobBinding to support deferred closing of connections

--- packages/client/src/useJobBinding.tsx
+++ packages/client/src/useJobBinding.tsx
@@ -5,6 +5,22 @@ import {
   bindNewJobToSocketIO,
 } from "./JobSocketIOClient";
 
+type DeferredClosedConn =
+  | {
+      status: "connected";
+      jobConn: Promise<JobSocketIOConnection>;
+      initiateDeferredClose: () => void;
+    }
+  | {
+      status: "closing";
+      cancelClose: () => Promise<JobSocketIOConnection>;
+    };
+
+const DEFERRED_CLOSED_CONN_CACHE: Record<
+  `${string}${"" | `[${string}]`}`,
+  DeferredClosedConn | undefined
+> = {};
+
 export function useJobBinding({
   socketIOURI,
   socketIOPath,
@@ -19,19 +35,41 @@ export function useJobBinding({
     status: "connecting",
   });
   const clientRef = useRef<Promise<JobSocketIOConnection>>();
+  const deferredCloseRef = useRef<() => void>();
 
   useEffect(() => {
     if (!clientRef.current) {
       try {
         setStatus({ status: "connecting" });
-        const connection = bindNewJobToSocketIO({
-          socketIOURI,
-          socketIOPath,
-          socketIOClient,
-          specName,
-          uniqueSpecLabel,
-        });
-        clientRef.current = connection;
+
+        const existing =
+          DEFERRED_CLOSED_CONN_CACHE[
+            `${specName}${uniqueSpecLabel ? `[${uniqueSpecLabel}]` : ""}`
+          ];
+
+        let connection: Promise<JobSocketIOConnection>;
+        if (existing) {
+          if (existing.status === "connected") {
+            connection = existing.jobConn;
+          } else {
+            connection = existing.cancelClose();
+          }
+        } else {
+          connection = bindNewJobToSocketIO({
+            socketIOURI,
+            socketIOPath,
+            socketIOClient,
+            specName,
+            uniqueSpecLabel,
+          });
+          const { initiateDeferredClose } = resetToConnected({
+            specName,
+            uniqueSpecLabel,
+            connection,
+          });
+          deferredCloseRef.current = initiateDeferredClose;
+        }
+
         connection.then((c) => {
           setStatus({
             status: "connected",
@@ -40,6 +78,7 @@ export function useJobBinding({
             uniqueSpecLabel,
           });
         });
+        clientRef.current = connection;
       } catch (error) {
         setStatus({ status: "error", errorMessage: JSON.stringify(error) });
         console.error("Failed to setup JobSocketIOConnection:", error);
@@ -47,14 +86,53 @@ export function useJobBinding({
     }
 
     return () => {
-      clientRef.current?.then((c) => {
-        c.close();
-      });
+      deferredCloseRef.current?.();
+      deferredCloseRef.current = undefined;
       clientRef.current = undefined;
     };
   }, [socketIOURI, socketIOPath, specName, uniqueSpecLabel]);
 
   return { ...status, connRef: clientRef };
+}
+
+function resetToConnected({
+  specName,
+  uniqueSpecLabel,
+  connection,
+}: {
+  specName: string;
+  uniqueSpecLabel?: string;
+  connection: Promise<JobSocketIOConnection>;
+}) {
+  const r = {
+    status: "connected" as const,
+    jobConn: connection,
+    initiateDeferredClose: () => {
+      console.log("Closing connection");
+      const timeout = setTimeout(async () => {
+        (await connection).close();
+        DEFERRED_CLOSED_CONN_CACHE[
+          `${specName}${uniqueSpecLabel ? `[${uniqueSpecLabel}]` : ""}`
+        ] = undefined;
+      }, 1000 * 5);
+
+      DEFERRED_CLOSED_CONN_CACHE[
+        `${specName}${uniqueSpecLabel ? `[${uniqueSpecLabel}]` : ""}`
+      ] = {
+        status: "closing",
+        cancelClose: async () => {
+          console.log("Cancelling close");
+          clearTimeout(timeout);
+          resetToConnected({ specName, uniqueSpecLabel, connection });
+          return connection;
+        },
+      };
+    },
+  };
+  DEFERRED_CLOSED_CONN_CACHE[
+    `${specName}${uniqueSpecLabel ? `[${uniqueSpecLabel}]` : ""}`
+  ] = r;
+  return r;
 }
 
 type JobStatus =
