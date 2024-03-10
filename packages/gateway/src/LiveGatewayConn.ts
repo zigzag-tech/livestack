56ce57e30206c54b53fb05107a077ad19c6091e3
Xiyang Chen <settinghead@gmail.com>
2024-03-09 19:42:58 -0500

    Refactor LiveGatewayConn class and add cleanup functions

--- packages/gateway/src/LiveGatewayConn.ts
+++ packages/gateway/src/LiveGatewayConn.ts
@@ -35,7 +35,7 @@ export class LiveGatewayConn {
     this.socket = socket;
     this.allowedSpecsForBinding = allowedSpecsForBinding;
 
-    addMethodResponder({
+    const cleanup = addMethodResponder({
       socket: this.socket,
       req: { method: REQUEST_AND_BIND_CMD },
       res: { method: MSG_JOB_INFO },
@@ -68,11 +68,27 @@ export class LiveGatewayConn {
       },
     });
 
-    this.socket.on(CMD_UNBIND, ({ jobId }: UnbindParams) => {
+    const that = this;
+    const feedListener = async <K extends keyof any>({
+      data,
+      tag,
+      jobId,
+    }: FeedParams<K, any>) => {
+      // console.info("Feeding data to job ", jobId, " with tag ", tag);
+      const { input } = that.jobFnsById[jobId];
+      try {
+        await input.byTag(tag).feed(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    this.socket.on(CMD_FEED, feedListener);
+    const unbindListener = async ({ jobId }: UnbindParams) => {
       const input = this.jobFnsById[jobId].input;
+
       for (const key of input.tags) {
         try {
-          input.byTag(key).terminate();
+          await input.byTag(key).terminate();
         } catch (err) {
           console.error(err);
         }
@@ -83,22 +99,15 @@ export class LiveGatewayConn {
       for (const sub of subs) {
         sub.unsubscribe();
       }
-    });
-    const that = this;
-    this.socket.on(
-      CMD_FEED,
-      async <K extends keyof any>({ data, tag, jobId }: FeedParams<K, any>) => {
-        // console.info("Feeding data to job ", jobId, " with tag ", tag);
-        const { input } = that.jobFnsById[jobId];
-        try {
-          await input.byTag(tag).feed(data);
-        } catch (err) {
-          console.error(err);
-        }
-      }
-    );
+
+      cleanup();
+      this.socket.off(CMD_FEED, feedListener);
+      this.socket.off(CMD_UNBIND, unbindListener);
+    };
+    this.socket.on(CMD_UNBIND, unbindListener);
   }
   private subsByJobId: Record<string, Subscription[]> = {};
+
   private jobFnsById: Record<
     string,
     {
@@ -165,11 +174,17 @@ function addMethodResponder<P, R>({
   socket: Socket;
   fn: (arg: P) => Promise<R>;
 }) {
-  socket.on(req.method, async (arg: { data: P; requestIdentifier: string }) => {
+  const listener = async (arg: { data: P; requestIdentifier: string }) => {
     const result = await fn(arg.data);
     socket.emit(res.method, {
       data: result,
       requestIdentifier: arg.requestIdentifier,
     });
-  });
+  };
+  socket.on(req.method, listener);
+
+  const cleanup = () => {
+    socket.off(req.method, listener);
+  };
+  return cleanup;
 }
