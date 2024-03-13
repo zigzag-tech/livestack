1c7dc3598850a60ef45cc87155fd1d9bf27b716c
Xiyang Chen <settinghead@gmail.com>
2024-03-13 23:02:50 +0000

    Refactor sum_as_string to unique_spec_identifier in shared-py/src/lib.rs

--- shared-py/src/lib.rs
+++ shared-py/src/lib.rs
@@ -1,17 +1,17 @@
 use pyo3::prelude::*;
 // use shared::systems::system_a:: { do_stuff_from_system_a }; // or (1)
 // use shared::systems::system_a::*; // or this one (1)
-use livestack_shared::systems::system_a::sum_as_string_impl;
+use livestack_shared::systems::def_graph_utils::{unique_spec_identifier as unique_spec_identifier_impl};
 /// Formats the sum of two numbers as string.
 
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok(sum_as_string_impl(a, b).to_string())
+fn unique_spec_identifier(a: String, b: Option<String>) -> PyResult<String> {
+    Ok(unique_spec_identifier_impl(a, b))
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn shared(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(unique_spec_identifier, m)?)?;
     Ok(())
 }
