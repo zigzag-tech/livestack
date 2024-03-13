f6ee56bb798e99822adf5504cc471dc3cc4dd645
Xiyang Chen <settinghead@gmail.com>
2024-03-13 22:36:18 +0000

    move all projects under packages/ to /

--- packages/shared/dist/livestack-shared.cjs.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "../src/index";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibGl2ZXN0YWNrLXNoYXJlZC5janMuZC50cyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSJ9
