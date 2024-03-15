ede9da373ebc8b5ecdc8f2c272b987f44d328e78
Xiyang Chen <xiyang@zztech.io>
2024-03-15 12:31:13 -0400

    add back shared wasm

--- shared-wasm/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wasm-pack build --out-dir ../shared-wasm-pkg --target nodejs
-# jq '.name = "@livestack/shared-wasm"' ../shared-wasm-pkg/package.json > tmp.$$.json && mv tmp.$$.json ../shared-wasm-pkg/package.json
