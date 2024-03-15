f79b26c74086deb3edcab74977ef6b932f2c8eb9
Xiyang Chen <xiyang@zztech.io>
2024-03-15 14:31:50 -0400

    Add shared-wasm package and build script

--- shared-wasm/build.sh
+++ shared-wasm/build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-wasm-pack build --out-dir ../shared-wasm-pkg --target nodejs
+
+# cd into the directory where the script is
+cd "$(dirname "$0")"
+
+# ensure pkg/src exists
+mkdir -p ./build_tmp/dist
+
+wasm-pack build --out-dir ./build_tmp --out-name dist/livestack_shared_wasm --target nodejs
 # jq '.name = "@livestack/shared-wasm"' ../shared-wasm-pkg/package.json > tmp.$$.json && mv tmp.$$.json ../shared-wasm-pkg/package.json
+rm ./build_tmp/.gitignore
+
+rsync -av ./build_tmp/ ./ --remove-source-files
+rm -rf ./build_tmp
