d1661105544e6f76320a3c25b22cb367e00d6479
Xiyang Chen <settinghead@gmail.com>
2024-03-20 12:21:15 -0400

    Refactor StreamDefSet to include extra fields

--- shared/src/StreamDefSet.ts
+++ shared/src/StreamDefSet.ts
@@ -37,27 +37,32 @@ export type InferDefMap<TMap> = {
 //   };
 // }
 
-export function createStreamDefSet<TMap>({
+export function createStreamDefSet<TMap, ExtraFields>({
   defs,
+  extraFields,
 }: {
   defs: InferDefMap<TMap>;
+  extraFields?: ExtraFields;
 }): {
-  streamDefSet: StreamDefSet<TMap>;
+  streamDefSet: StreamDefSet<TMap, ExtraFields>;
   tags: (keyof TMap)[];
 } {
   const tags = Object.keys(defs) as (keyof TMap)[];
-  const streamDefSet = {} as StreamDefSet<TMap>;
+  const streamDefSet = {} as StreamDefSet<TMap, ExtraFields>;
   for (const tag of tags) {
-    streamDefSet[tag] = { tag, def: defs[tag] };
+    streamDefSet[tag] = { tag, def: defs[tag], ...extraFields } as StreamDefSet<
+      TMap,
+      ExtraFields
+    >[keyof TMap];
   }
   return { streamDefSet, tags };
 }
 
-export type StreamDefSet<TMap> = {
-  [K in keyof TMap]: TaggedStreamDef<K, TMap[K]>;
+export type StreamDefSet<TMap, ExtraFields = {}> = {
+  [K in keyof TMap]: TaggedStreamDef<K, TMap[K], ExtraFields>;
 };
 
-export type TaggedStreamDef<K, T> = {
+export type TaggedStreamDef<K, T, ExtraFields> = {
   tag: K;
   def: z.ZodType<T>;
-};
+} & ExtraFields;
