06d00db943811819ff2e3bbb3332b7ffcd46403c
Xiyang Chen <settinghead@gmail.com>
2024-03-21 00:06:45 -0400

    Add @livestack/shared-wasm as a dependency

--- shared-wasm/build.sh
+++ shared-wasm/build.sh
@@ -17,7 +17,7 @@ rm -rf ./build_tmp
 # ensure pkg/src exists
 mkdir -p ./build_tmp_nodejs/
 wasm-pack build --out-dir ./build_tmp_nodejs --out-name livestack_shared_wasm_nodejs --target nodejs
-# jq '.name = "@livestack/shared-wasm"' ../shared-wasm-pkg/package.json > tmp.$$.json && mv tmp.$$.json ../shared-wasm-pkg/package.json
+jq '.name = "@livestack/shared-wasm"' ../shared-wasm/package.json > tmp.$$.json && mv tmp.$$.json ../shared-wasm/package.json
 rm ./build_tmp_nodejs/.gitignore
 
 rsync -av ./build_tmp_nodejs/ ../shared/src/graph_wasm --remove-source-files
