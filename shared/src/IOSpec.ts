ec5020d7acd9dd42d46c7ad285bd17e9cf7647e1
Xiyang Chen <xiyang@zztech.io>
2024-04-06 18:41:21 -0400

    Add invalid type and check function

--- shared/src/IOSpec.ts
+++ shared/src/IOSpec.ts
@@ -102,3 +102,21 @@ export function wrapIfSingle<
     return def as unknown as TMap;
   }
 }
+
+// credit: https://github.com/ryb73/invalid-type
+
+const InvalidTypeSymbol = Symbol(`Invalid type`);
+export type invalid<ErrorMessage> =
+  | ((
+      // ErrorMessage doesn't need to be used here, except that using it allows
+      // TypeScript to print the passed message instead of just "ErrorMessage"
+      // in certain cases.
+      invalidType: ErrorMessage & typeof InvalidTypeSymbol,
+      ..._: (typeof InvalidTypeSymbol)[]
+    ) => typeof InvalidTypeSymbol)
+  | null
+  | undefined;
+
+type Check<C> = C extends number
+  ? number
+  : invalid<"This is not very printer friendly sir.">;
