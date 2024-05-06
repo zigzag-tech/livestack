25c5f788691397392739b62ca87b5246f393375b
Xiyang Chen <xiyang@zztech.io>
2024-05-06 18:15:28 -0400

    make p2p-storage esm

--- storage-p2p/src/p2p-storage-provider.ts
+++ storage-p2p/src/p2p-storage-provider.ts
@@ -1,19 +1,19 @@
-import {
+import type {
   IStorageProvider,
   InferRestoredFileType,
   LargeFileWithoutValue,
   OriginalType,
 } from "@livestack/core";
-import { createLibp2p } from "libp2p";
-import { tcp } from "@libp2p/tcp";
-import { mplex } from "@libp2p/mplex";
-import { noise } from "@chainsafe/libp2p-noise";
-import { kadDHT } from "@libp2p/kad-dht";
-import { pubsubPeerDiscovery } from "@libp2p/pubsub-peer-discovery";
-import { toString as uint8ArrayToString } from "uint8arrays/to-string";
-import { fromString as uint8ArrayFromString } from "uint8arrays/from-string";
-import { bootstrap } from "@libp2p/bootstrap";
-import { concat } from "uint8arrays/concat";
+// import { createLibp2p } from "libp2p";
+// import { tcp } from "@libp2p/tcp";
+// import { mplex } from "@libp2p/mplex";
+// import { noise } from "@chainsafe/libp2p-noise";
+// import { kadDHT } from "@libp2p/kad-dht";
+// import { pubsubPeerDiscovery } from "@libp2p/pubsub-peer-discovery";
+// import { toString as uint8ArrayToString } from "uint8arrays/to-string.node";
+// import { fromString as uint8ArrayFromString } from "uint8arrays/from-string";
+// import { bootstrap } from "@libp2p/bootstrap";
+// import { concat } from "uint8arrays/concat";
 
 let vaultServerURL = "localhost:50504";
 
@@ -26,6 +26,23 @@ const vaultServerHost = vaultServerURL.split(":")[0];
 export async function getP2PStorageProvider(
   networkId: string
 ): Promise<IStorageProvider> {
+  const { createLibp2p } = await import("libp2p");
+  const { tcp } = await import("@libp2p/tcp");
+  const { mplex } = await import("@libp2p/mplex");
+  const { noise } = await import("@chainsafe/libp2p-noise");
+  const { kadDHT } = await import("@libp2p/kad-dht");
+  // const { pubsubPeerDiscovery } = await import("@libp2p/pubsub-peer-discovery");
+  const { bootstrap } = await import("@libp2p/bootstrap");
+  const uint8arrays = (await import("uint8arrays")).default;
+
+  // const { fromString: uint8ArrayFromString } = await import(
+  //   "uint8arrays/from-string"
+  // );
+  // const { toString: uint8ArrayToString } = await import(
+  //   "uint8arrays/to-string.node"
+  // );
+  const { concat } = await import("uint8arrays/concat");
+
   const bootstrapMultiaddr = `/ip4/${vaultServerHost}/tcp/65448/p2p/QmBootstrapNodeID`;
 
   const node = await createLibp2p({
@@ -41,9 +58,9 @@ export async function getP2PStorageProvider(
         list: [bootstrapMultiaddr],
         tagName: `myapp-${networkId}`,
       }),
-      pubsubPeerDiscovery({
-        topics: [`${networkId}.storage-p2p`],
-      }),
+      // pubsubPeerDiscovery({
+      //   topics: [`${networkId}.storage-p2p`],
+      // }),
     ],
     services: {
       dht: kadDHT({
@@ -56,9 +73,9 @@ export async function getP2PStorageProvider(
 
   return {
     putToStorage: async (destination, data) => {
-      const encodedData = uint8ArrayFromString(data.toString());
+      const encodedData = uint8arrays.fromString(data.toString());
       await node.contentRouting.put(
-        uint8ArrayFromString(destination),
+        uint8arrays.fromString(destination),
         encodedData
       );
     },
@@ -66,13 +83,13 @@ export async function getP2PStorageProvider(
       f: LargeFileWithoutValue<T>
     ): Promise<InferRestoredFileType<T>> => {
       const asyncIterable = node.contentRouting.get(
-        uint8ArrayFromString(f.path)
+        uint8arrays.fromString(f.path)
       );
       const result = await asyncIterable;
       const data = concat([result]);
 
       if (f.originalType === "string") {
-        return uint8ArrayToString(data) as InferRestoredFileType<T>;
+        return uint8arrays.toString(data) as InferRestoredFileType<T>;
       } else if (f.originalType === "buffer") {
         return data as unknown as InferRestoredFileType<T>;
       } else if (f.originalType === "array-buffer") {
