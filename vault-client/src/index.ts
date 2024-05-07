2843bd377d57dc3161b1d31d8b754c25ec370f67
Xiyang Chen <xiyang@zztech.io>
2024-05-06 21:42:46 -0400

    refactor: enable local vault dev server for development environment

--- vault-client/src/index.ts
+++ vault-client/src/index.ts
@@ -36,19 +36,20 @@ export function findSuitableVaultServer(authToken: string) {
   let vaultServerURL: string;
   if (process.env.LIVESTACK_VALULT_SERVER_URL) {
     vaultServerURL = process.env.LIVESTACK_VALULT_SERVER_URL;
-  } else if (process.env.LAUNCH_LOCAL_VAULT_DEV_SERVER) {
-    try {
-      require.resolve("@livestack/vault-dev-server");
-      const main = require("@livestack/vault-dev-server");
-      const port = 50508;
-      main.launchVaultDevServer(port);
-      vaultServerURL = `localhost:${port}`;
-    } catch (e) {
-      console.log(e);
-      throw new Error(
-        "Vault dev server module @livestack/vault-dev-server not installed or failed to launch. Aborting app."
-      );
-    }
+    // } else if (process.env.LAUNCH_LOCAL_VAULT_DEV_SERVER) {
+    //   try {
+    //     require.resolve("@livestack/vault-dev-server");
+    //     const main = require("@livestack/vault-dev-server");
+    //     const port = 50508;
+    //     main.launchVaultDevServer(port);
+    //     vaultServerURL = `localhost:${port}`;
+    //   } catch (e) {
+    //     console.log(e);
+    //     throw new Error(
+    //       "Vault dev server module @livestack/vault-dev-server not installed or failed to launch. Aborting app."
+    //     );
+    //   }
+    // } else {
   } else {
     vaultServerURL = "livedev.zztech.io:50504";
   }
