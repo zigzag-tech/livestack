2bd2243f22f66c26b5c12f2fb88c224c31cf819d
Xiyang Chen <xiyang@zztech.io>
2024-05-08 00:14:05 -0400

    refactor: update S3 storage provider to use @aws-sdk/client-s3
    This commit updates the S3 storage provider to use the `@aws-sdk/client-s3` package instead of the deprecated `aws-sdk` package. The code changes include importing the necessary client and command classes from `@aws-sdk/client-s3` and updating the method calls to use the new client instance. This update ensures compatibility with the latest version of the AWS SDK and improves the maintainability of the codebase.

--- storage-s3/src/s3-storage-provider.ts
+++ storage-s3/src/s3-storage-provider.ts
@@ -1,11 +1,22 @@
-import AWS from "aws-sdk";
+import {
+  GetObjectCommandInput,
+  GetObjectCommandOutput,
+  PutObjectCommandInput,
+  PutObjectCommandOutput,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  HeadObjectCommand,
+} from "@aws-sdk/client-s3";
+
 import {
   IStorageProvider,
   InferRestoredFileType,
   OriginalType,
   calculateHash,
 } from "@livestack/core";
-import crypto from "crypto";
 import { Readable, Stream } from "stream";
 
 export function getAWSS3StorageProvider({
@@ -21,13 +32,21 @@ export function getAWSS3StorageProvider({
   // accessKeySecret: string;
   cdnPrefix?: string;
 }): IStorageProvider {
-  const s3 = new AWS.S3({});
+  const s3client = new S3Client({});
 
   return {
     putToStorage: async (destination, data) => {
       if (data instanceof Stream) {
         // stream is not hashable, upload directly
-        await s3.upload({ Bucket: bucketName, Key: destination, Body: data }).promise();
+        const readable = data as Readable;
+        await s3client.send<PutObjectCommandInput, PutObjectCommandOutput>(
+          new PutObjectCommand({
+            Bucket: bucketName,
+            Key: destination,
+            Body: readable,
+          })
+        );
+
         return;
       }
       const hash = await calculateHash(data);
@@ -35,7 +54,9 @@ export function getAWSS3StorageProvider({
       // Check if a file with the same hash already exists
       const hashRefPath = `__hash_refs/${hash}`;
       try {
-        await s3.headObject({ Bucket: bucketName, Key: hashRefPath }).promise();
+        await s3client.send(
+          new HeadObjectCommand({ Bucket: bucketName, Key: hashRefPath })
+        );
         // File with the same hash already exists, skip uploading
         return { hash };
       } catch (error: any) {
@@ -47,10 +68,28 @@ export function getAWSS3StorageProvider({
 
       // File with the same hash doesn't exist, proceed with uploading
       const body = Buffer.isBuffer(data) ? data : Buffer.from(data.toString());
-      await s3.putObject({ Bucket: bucketName, Key: destination, Body: body }).promise();
+      // await s3
+      //   .putObject({ Bucket: bucketName, Key: destination, Body: body })
+      //   .promise();
+      await s3client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: destination,
+          Body: body,
+        })
+      );
 
       // Store the hash reference file
-      await s3.putObject({ Bucket: bucketName, Key: hashRefPath, Body: destination }).promise();
+      // await s3
+      //   .putObject({ Bucket: bucketName, Key: hashRefPath, Body: destination })
+      //   .promise();
+      await s3client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: hashRefPath,
+          Body: Buffer.from(destination),
+        })
+      );
 
       return { hash };
     },
@@ -62,24 +101,37 @@ export function getAWSS3StorageProvider({
     }) => {
       let result;
       try {
-        result = await s3
-          .getObject({ Bucket: bucketName, Key: f.path })
-          .promise();
+        // result = await s3
+        //   .getObject({ Bucket: bucketName, Key: f.path })
+        //   .promise();
+        result = await s3client.send(
+          new GetObjectCommand({ Bucket: bucketName, Key: f.path })
+        );
       } catch (error: any) {
         if (error.code === "NoSuchKey" && f.hash) {
           // If the file is not found and a hash is provided, look up the hash reference
           const hashRefPath = `__hash_refs/${f.hash}`;
           try {
-            const hashRefResult = await s3
-              .getObject({ Bucket: bucketName, Key: hashRefPath })
-              .promise();
+            // const hashRefResult = await s3
+            //   .getObject({ Bucket: bucketName, Key: hashRefPath })
+            //   .promise();
+            const hashRefResult = await s3client.send<
+              GetObjectCommandInput,
+              GetObjectCommandOutput
+            >(new GetObjectCommand({ Bucket: bucketName, Key: hashRefPath }));
             const referencedPath = hashRefResult.Body?.toString();
             if (!referencedPath) {
               throw new Error("No data returned from S3");
             }
-            result = await s3
-              .getObject({ Bucket: bucketName, Key: referencedPath })
-              .promise();
+            // result = await s3
+            //   .getObject({ Bucket: bucketName, Key: referencedPath })
+            //   .promise();
+            result = await s3client.send<
+              GetObjectCommandInput,
+              GetObjectCommandOutput
+            >(
+              new GetObjectCommand({ Bucket: bucketName, Key: referencedPath })
+            );
           } catch (hashRefError: any) {
             console.log(
               "Error fetching hash reference",
@@ -96,21 +148,27 @@ export function getAWSS3StorageProvider({
 
       // Assuming result.Body is a Buffer if not streaming
       if (f.originalType === "string") {
-        return result.Body.toString("utf-8") as R; // Ensure encoding is specified if needed
+        return (await result.Body.transformToString("utf-8")) as R; // Ensure encoding is specified if needed
       } else if (f.originalType === "buffer") {
-        return result.Body as R;
+        return Buffer.from(await result.Body.transformToByteArray()) as R;
       } else if (f.originalType === "array-buffer") {
-        return (result.Body as Buffer).buffer as R;
+        return (await result.Body.transformToByteArray()) as R;
       } else if (f.originalType === "blob") {
         // Here, we must ensure the Buffer type is correctly handled.
-        const blob = new Blob([new Uint8Array(result.Body as Buffer)]);
+        const blob = new Blob([
+          new Uint8Array(Buffer.from(await result.Body.transformToByteArray())),
+        ]);
         return blob as R;
       } else if (f.originalType === "file") {
-        const blob = new Blob([new Uint8Array(result.Body as Buffer)]);
+        const blob = new Blob([
+          new Uint8Array(Buffer.from(await result.Body.transformToByteArray())),
+        ]);
         return new File([blob], f.path) as R;
       } else if (f.originalType === "stream") {
         // Readable.from should be used directly on Buffer
-        return Readable.from(result.Body as Buffer) as Readable as R;
+        return Readable.from(
+          Buffer.from(await result.Body.transformToByteArray())
+        ) as Readable as R;
       } else {
         throw new Error("Unsupported originalType " + f.originalType);
       }
