1a23cd72345f2cc27777fc1c1c9b18f15a90a7af
Xiyang Chen <xiyang@zztech.io>
2024-05-14 20:06:18 -0400

    chore: Add `useStream` hook for streaming data
    The `useStream` hook has been added to handle streaming data in the client application. This hook allows for subscribing to a stream of data based on the provided job information, type, and query. It manages the state of the output data and provides a convenient way to handle different types of queries.

--- client/src/useOutput.tsx
+++ client/src/useOutput.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
 import { z } from "zod";
 import { JobInfo } from "./useJobBinding";
 import { StreamQuery } from "@livestack/shared";
+import { useStream } from "./useStream";
 
 export function useOutput<O>({
   job: { specName, uniqueSpecLabel, jobId, connRef },
@@ -21,79 +21,4 @@ export function useOutput<O>({
     type: "output",
     query,
   });
-}
-
-export function useStream<O>({
-  job: { specName, uniqueSpecLabel, jobId, connRef },
-  tag,
-  type,
-  query,
-}: {
-  job: JobInfo<any>;
-  type: "input" | "output";
-  tag?: string;
-  def?: z.ZodType<O>;
-  query: StreamQuery;
-}) {
-  const [output, setOutput] = useState<
-    {
-      data: O;
-      timestamp: number;
-      chunkId: string;
-      tag?: string;
-    }[] & {
-      last: {
-        data: O;
-        timestamp: number;
-        chunkId: string;
-        tag?: string;
-      } | null;
-    }
-  >(Object.assign([], { last: null }));
-
-  useEffect(() => {
-    let unsubP: Promise<() => void> | null = null;
-    if (specName && jobId) {
-      const conn = connRef.current;
-      if (!conn) {
-        throw new Error(`Connection not found with jobId "${jobId}".`);
-      }
-
-      unsubP = conn.then((conn) =>
-        conn.subToStream<O>(
-          {
-            tag,
-            type,
-            query,
-          },
-          (data) => {
-            if (query.type === "lastN") {
-              setOutput((prev) => {
-                if (prev.length >= query.n) {
-                  return Object.assign([...prev.slice(1), data], {
-                    last: data,
-                  });
-                }
-                return Object.assign([...prev, data], { last: data });
-              });
-            } else if (query.type === "all") {
-              setOutput((prev) =>
-                Object.assign([...prev, data], { last: data })
-              );
-            } else {
-              throw new Error(`Unsupported query type "${query.type}".`);
-            }
-          }
-        )
-      );
-    }
-
-    return () => {
-      if (unsubP) {
-        unsubP.then((unsub) => unsub());
-      }
-    };
-  }, [specName, uniqueSpecLabel, jobId, tag]);
-
-  return output;
 }
