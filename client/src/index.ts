13ae2a3725a1ab660f13d234a7ff4eb34889c2e7
Xiyang Chen <xiyang@zztech.io>
2024-05-15 20:58:03 -0400

    remove useCumulative

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,5 +2,4 @@ export { useJobBinding } from "./useJobBinding";
 export { useInput } from "./useInput";
 export { useOutput } from "./useOutput";
 export * from "./useStream";
-export { useCumulative } from "./useCumulative";
 export type { JobInfo } from "./useJobBinding";
