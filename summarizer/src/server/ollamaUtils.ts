eb35aa5f351e61e93688755c4a03912301ceb25e
Xiyang Chen <xiyang@zztech.io>
2024-05-23 16:12:05 -0400

    llama3

--- summarizer/src/server/ollamaUtils.ts
+++ summarizer/src/server/ollamaUtils.ts
@@ -1,7 +1,7 @@
 import { wrapWithTransientStdout } from "@livestack/shared";
 import { Message } from "ollama";
 
-const CONVO_MODEL = "mistral:v0.3";
+const CONVO_MODEL = "llama3:instruct";
 
 export async function generateSimpleResponseOllama({
   messages,
