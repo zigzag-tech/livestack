a590e250c623d117654baa269f62b83dea1525b4
Emily Wang <yiwang9264@gmail.com>
2024-06-05 04:12:32 +0000

    tutorial

--- examples/speech-app/vite.config.ts
+++ examples/speech-app/vite.config.ts
@@ -5,9 +5,4 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
-  server: {
-    fs: {
-      strict: false,
-    },
-  },
 });
