4cea25840e3ab2b81af5319b8bd8865b9393af91
Emily Wang <yiwang9264@gmail.com>
2024-07-03 02:44:02 +0000

    text splitter **breaking change**

--- lab-server/src/text-splitting-worker.ts
+++ lab-server/src/text-splitting-worker.ts
@@ -7,9 +7,15 @@ const AUTO_START_WORKER = true;
 
 export const textSplittingSpec = JobSpec.define({
   name: "text-splitting",
-  input: z.string(),
+  input: z.object({
+    documentId: z.string(),
+    content: z.string(),
+  }),
   output: {
-    default: z.string(),
+    default: z.object({
+      chunk: z.string(),
+      documentId: z.string(),
+    }),
     "splitting-stats": splittingStatsSchema,
   },
   jobOptions: z.object({
@@ -20,22 +26,38 @@ export const textSplittingSpec = JobSpec.define({
 export const textSplittingWorkerDef = textSplittingSpec.defineWorker({
   autostartWorker: AUTO_START_WORKER,
   processor: async ({ input, output, jobOptions }) => {
-    const splitter = await genSplitterIterable({
-      chunkSize: jobOptions.chunkSize || DEFAULT_CHUNK_SIZE,
-    });
-    (async () => {
-      for await (const chunk of input) {
-        const { numCharsInCache } = await splitter.feed(chunk);
-        await output("splitting-stats").emit({
-          numCharsInCache,
-          total: DEFAULT_CHUNK_SIZE,
-        });
-      }
-      // await splitter.flushRemaining();
-    })();
+    let currentDocumentId : string | null = null;
+    let splitter : ReturnType<typeof genSplitterIterable> | null = null;
 
-    for await (const chunk of splitter) {
-      await output.emit(chunk);
+    for await (const { content: chunk, documentId } of input) {
+      if(currentDocumentId !== documentId) {
+        // flush the current splitter
+        splitter?.flushRemaining();
+
+        // create new splitter for the new document
+        splitter = genSplitterIterable({
+          chunkSize: jobOptions.chunkSize || DEFAULT_CHUNK_SIZE,
+        });
+        currentDocumentId = documentId;
+
+        // start new emit loop 
+        (async () => {
+          for await (const chunk of splitter) {
+            await output.emit({
+              documentId,
+              chunk, 
+            });
+          }
+        })();
+      }
+      
+      const { numCharsInCache } = await splitter!.feed(chunk);
+      await output("splitting-stats").emit({
+        numCharsInCache,
+        total: DEFAULT_CHUNK_SIZE,
+      });
     }
+
+    
   },
 });
