2212431d00523852480d49e6c6d6c28a2f076f61
allen233333 <1657054849@qq.com>
2024-09-26 19:01:07 -0500

    Comment out process.stdout.write at std-utils

--- shared/src/std-utils.ts
+++ shared/src/std-utils.ts
@@ -5,16 +5,16 @@ export async function wrapWithTransientStdout(
 ) {
   let message = "";
   for await (const part of response) {
-    process.stdout.write(
-      part.message.content.replace("\n", " ").replace("\r", " ")
-    );
+    // process.stdout.write(
+    //   part.message.content.replace("\n", " ").replace("\r", " ")
+    // );
     message += part.message.content;
   }
   // erase all of what was written
   // Move the cursor to the beginning of the line
-  process.stdout.write("\r");
+  // process.stdout.write("\r");
   // process.stdout.write("\r\n");
   // Clear the entire line
-  process.stdout.write("\x1b[2K");
+  // process.stdout.write("\x1b[2K");
   return message;
 }
