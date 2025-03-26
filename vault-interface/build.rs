9b22c18e57852f61d21da0656a4d46324eb34f08
Xiyang Chen <settinghead@gmail.com>
2025-03-25 22:40:49 -0400

    chore: Add debug assertion configuration in build script and update Cargo.toml
    This commit adds a debug assertion configuration to the build script in build.rs, enabling conditional compilation for debug assertions. Additionally, it updates the Cargo.toml file to include a new "noop" feature under the features section, enhancing the project's modular capabilities.

--- vault-interface/build.rs
+++ vault-interface/build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rustc-check-cfg=cfg(debug_assert)");
     tonic_build::compile_protos("proto/stream.proto")?;
     napi_build::setup();
     Ok(())
