11086568edfd9a0323f195dbd4355c22966322d3
Xiyang Chen <settinghead@gmail.com>
2025-04-13 13:43:47 -0400

    refactor: Update import paths in service.ts and index.ts for cleaner module structure
    This commit refactors the import statements in service.ts to simplify the path for the vault-interface module. Additionally, it consolidates the export of types in index.ts, enhancing clarity and maintainability of the codebase.

--- vault-interface/src/index.ts
+++ vault-interface/src/index.ts
@@ -10,10 +10,10 @@ export { CacapcityServiceDefinition } from "./generated/capacity";
 
 import type { DBServiceImplementation } from "./generated/db";
 import type { QueueServiceImplementation } from "./generated/queue";
-import type { StreamServiceImplementation } from "./generated/stream";
+import type { StreamServiceImplementation, ValuesByReverseIndexRequest, LastValueRequest, SubRequest, SubType, AllValuesRequest, StreamDatapoint, JobInfo, StreamPubMessage } from "./generated/stream";
 import type { CacapcityServiceImplementation } from "./generated/capacity";
 
-export { StreamServiceImplementation };
+export { StreamServiceImplementation, ValuesByReverseIndexRequest, LastValueRequest, SubRequest, AllValuesRequest, StreamPubMessage };
 export { QueueServiceImplementation };
 export { DBServiceImplementation };
 export { CacapcityServiceImplementation };
