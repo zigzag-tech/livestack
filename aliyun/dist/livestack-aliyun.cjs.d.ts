c8c89c1e2f0cb20c6c9a19da1d976734550c6c7c
Xiyang Chen <settinghead@gmail.com>
2026-05-20 20:39:12 +0800

    Stop tracking Aliyun package dist

--- aliyun/dist/livestack-aliyun.cjs.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./declarations/src/index.js";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibGl2ZXN0YWNrLWFsaXl1bi5janMuZC50cyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4vZGVjbGFyYXRpb25zL3NyYy9pbmRleC5kLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBIn0=
--- aliyun/dist/livestack-aliyun.cjs.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./livestack-aliyun.cjs.prod.js");
-} else {
-  module.exports = require("./livestack-aliyun.cjs.dev.js");
-}
